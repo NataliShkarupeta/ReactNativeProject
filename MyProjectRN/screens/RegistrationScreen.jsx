f06489c3120995c8cf320767b1bce0201dd23f22
Nataliia Shkarupeta <natalinardi.kh@gmail.com>
2023-05-09 18:21:25 +0300

    add register form

--- MyProjectRN/screens/RegistrationScreen.jsx
+++ MyProjectRN/screens/RegistrationScreen.jsx
@@ -1,16 +1,25 @@
 import {useCallback, useEffect} from 'react';
 import {useForm} from 'react-hook-form';
-import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
+  ScrollView,
+} from 'react-native';
 
 export const RegistrationScreen = () => {
   const {register, handleSubmit, setValue} = useForm();
-  console.log('register', register);
-  console.log('handleSubmit', handleSubmit);
-  console.log('setValue', setValue);
 
   const onSubmit = useCallback(formData => {
     console.log(formData);
   }, []);
+
   const onChangeField = useCallback(
     name => text => {
       setValue(name, text);
@@ -22,44 +31,92 @@ export const RegistrationScreen = () => {
     register('email');
     register('password');
   }, [register]);
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Реєстрація</Text>
-      <View style={styles.box}>
-        <TextInput
-          style={styles.input}
-          secureTextEntry
-          autoCompleteType="password"
-          placeholder="Логін"
-          onChangeText={onChangeField('password')}
-        />
-        <TextInput
-          style={styles.input}
-          autoCompleteType="email"
-          keyboardType="email-address"
-          textContentType="emailAddress"
-          placeholder="адреса електроонної пошти"
-          onChangeText={onChangeField('email')}
-        />
-        <TextInput
-          style={styles.input}
-          secureTextEntry
-          autoCompleteType="password"
-          placeholder="Пароль"
-          onChangeText={onChangeField('password')}
-        />
-      </View>
 
-      <Button
-        style={styles.buttonSubmit}
-        title="Зареєструватись"
-        onPress={handleSubmit(onSubmit)}
-      />
-    </View>
+
+  return (
+    <ScrollView>
+      {/* <Icon name="rocket" size={30} color="#900" /> */}
+      {/* <Icon.Button /> */}
+      <View style={styles.container}>
+      
+
+
+        <View style={styles.boxForPhoto}></View>
+        <Text style={styles.welcome}>Реєстрація</Text>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={styles.containerKey}>
+          {/* <View style={styles.box}> */}
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.box}>
+              <TextInput
+                style={styles.input}
+                autoCompleteType="text"
+                placeholder="Логін"
+                keyboardType="text"
+                onChangeText={onChangeField('password')}
+              />
+
+              <TextInput
+                style={styles.input}
+                autoCompleteType="email"
+                keyboardType="email-address"
+                textContentType="emailAddress"
+                placeholder="адреса електроонної пошти"
+                onChangeText={onChangeField('email')}
+              />
+              <View style={styles.boxPassword}>
+                <TextInput
+                  style={styles.input}
+                  secureTextEntry
+                  autoCompleteType="password"
+                  placeholder="Пароль"
+                  onChangeText={onChangeField('password')}></TextInput>
+                {/* <Text>Показати</Text> */}
+              </View>
+              <View style={styles.buttonSubmit}>
+                <Button
+                  title="Зареєструватись"
+                  onPress={handleSubmit(onSubmit)}
+                  color="#FFFFFF"
+                />
+              </View>
+              <Text style={styles.loginText}>Вже є акаунт? Увійти</Text>
+            </View>
+          </TouchableWithoutFeedback>
+          {/* </View> */}
+        </KeyboardAvoidingView>
+      </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
+  boxForPhoto: {
+    height: 120,
+    width: 120,
+    backgroundColor: '#F6F6F6',
+    textAlign: 'center',
+    marginTop: -60,
+    borderRadius:16,
+  },
+  loginText: {
+    color: '#1B4371',
+    textAlign: 'center',
+  },
+  boxPassword: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 43,
+  },
+  containerKey: {
+    flex: 1,
+  },
   buttonSubmit: {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+    fontSize: 16,
+    lineHeight: 1.18,
+    textAlign: 'center',
     height: 50,
     backgroundColor: '#FF6C00',
     width: 343,
@@ -71,8 +128,9 @@ const styles = StyleSheet.create({
   },
   container: {
     borderTopLeftRadius: 25,
-    borderTopRightRadius:25,
-    marginTop: 263,
+    borderTopRightRadius: 25,
+    // marginTop: 263,
+    marginTop: 163,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -81,7 +139,7 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 30,
     textAlign: 'center',
-    margin: 10,
+    marginTop: 32,
     fontWeight: 500,
     lineHeight: 35,
     marginBottom: 32,
@@ -90,8 +148,8 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   input: {
+    padding: 10,
     height: 50,
-    marginHorizontal: 16,
     backgroundColor: '#F6F6F6',
     borderWidth: 1,
     borderColor: '#E8E8E8',
